22670899b71258175810ad0ddb4d3d330c4eb29c
Lon4th <epsilonfortz@gmail.com>
2024-03-28 20:44:22 +0200

    Mod created

---
+++ src/main/java/net/eps/lonsbattletowers/LonsBattleTowersClient.java
@@ -0,0 +1,10 @@
+package net.eps.lonsbattletowers;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LonsBattleTowersClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
